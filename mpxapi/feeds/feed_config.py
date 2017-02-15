a12887fe2f44ae5acbdfd164c50d8f891923a4b6
Loke Dupont <loke@lokedupont.info>
2017-02-15 16:19:46 +0100

    More ways to use feeds service, and bump version

--- mpxapi/feeds/feed_config.py
+++ mpxapi/feeds/feed_config.py
@@ -1,3 +1,6 @@
+import json
+
+
 class FeedConfig:
     def __init__(self, api):
         self.schema = "2.2.0"
@@ -8,9 +11,19 @@ class FeedConfig:
     def get(self, params):
         return self.api.command(service=self.service, path=self.path, method="GET", params=self.apply_schema(params))
 
-    def get_by_id(self, id, params = {}):
+    def get_by_id(self, id, params={}):
         return self.api.command(service=self.service, path=self.path + '/' + str(id), method="GET",
                                 params=self.apply_schema(params))
+
+    def put_data(self, data, params={}):
+        return self.api.command(service=self.service, path=self.path + '/', method="PUT",
+                                params=self.apply_schema(params),
+                                data=json.dumps(data, sort_keys=True))
+
+    def post_data(self, data, params={}):
+        return self.api.command(service=self.service, path=self.path + '/', method="POST",
+                                params=self.apply_schema(params),
+                                data=json.dumps(data, sort_keys=True))
 
     def apply_schema(self, params):
         params.update({"schema": self.schema})
