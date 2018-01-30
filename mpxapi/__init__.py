77568518c32d7c9d53ce1a49e72e9770e0128a0f
Loke Dupont <loke@lokedupont.info>
2018-01-30 13:35:44 +0100

    Ignore pypi package files

--- mpxapi/__init__.py
+++ mpxapi/__init__.py
@@ -2,5 +2,5 @@ from .http import MPXApi
 from .api_base import get_guid_based_id
 
 
-__version__ = '0.0.16'
+__version__ = '0.1.0'
 __title__ = 'mpxapi'
--- mpxapi/http.py
+++ mpxapi/http.py
@@ -31,13 +31,13 @@ class MPXApi:
 
     def sign_in(self):
         # Ask for a default timeout of 1 hour, and 10 minutes of idle timeout
-        params = {'schema': '1.1', '_duration': '3600000', '_idleTimeout': '600000'}
+        params = {'schema': '1.1', '_duration': '3600000', '_idleTimeout': '600000', 'httpError': 'true'}
         headers = {'Content-Type': 'application/json'}
         auth = HTTPBasicAuth(username=self.username, password=self.password)
         r = requests.get(SIGN_IN_URL.format(tld=self.tld), params=params,
                          headers=headers, auth=auth)
 
-        if r.status_code == 200 and "AuthenticationException" not in r.text:
+        if r.status_code == 200:
             auth_data = r.json()['signInResponse']
             self.token = auth_data['token']
             self.user_id = '/'.join(auth_data['userId'].split('/')[-2:])
@@ -63,6 +63,7 @@ class MPXApi:
     def raw_command(self, method, url, params, data=None):
         params.update({"account": self.account})
         params.update({"token": self.token})
+        params.update({"httpError", "true"})
         if "form" not in params:
             params.update({"form": "cjson"})
             params.update({"pretty": "true"})
@@ -70,8 +71,8 @@ class MPXApi:
         req = requests.request(method, url, params=params, data=data)
 
         # check if we maybe have an expired token
-        if "AuthenticationException" in req.text or "InvalidTokenException" in req.text:
-            logging.debug("We encountered an AuthenticationException or InvalidTokenException, re-signing in")
+        if req.status_code == 401:
+            logging.debug("We encountered an 401 exception, re-signing in")
             self.sign_in()
             return self.raw_command(method, url, params, data)
 
